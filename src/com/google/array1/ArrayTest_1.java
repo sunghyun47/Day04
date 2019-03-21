5b813f62db1b710fdee5f088f76a879b20dc8cc5
sunghyun47 <034pps@naver.com>
2019-03-21 14:18:21 +0900

    add print

--- src/com/google/array1/ArrayTest_1.java
+++ src/com/google/array1/ArrayTest_1.java
@@ -3,8 +3,10 @@ package com.google.array1;
 public class ArrayTest_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Array Test");
+		
+		
 	}
 
 }
